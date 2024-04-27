9844d5062441f0fc67a24cb65e0b73bba848f653
dzeleniak <zeleniak.david@gmail.com>
2024-04-27 15:00:45 -0700

    entrypoint for the repl

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"gorangutang/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s...\n\nWelcome to Gorangutang\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
