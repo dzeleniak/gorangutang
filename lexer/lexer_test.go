b095298c8e47beb3a0e762966dd29a4cb2840e2e
dzeleniak <zeleniak.david@gmail.com>
2024-04-28 13:13:30 -0700

    refactor lexer tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,12 +5,28 @@ import (
 	"testing"
 )
 
-func TestNextToken(t *testing.T) {
+type LexerTestCase struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
+func (tc LexerTestCase) Check(l *Lexer, i int, t *testing.T) {
+	tok := l.NextToken()
+
+	if tok.Type != tc.expectedType {
+		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			i, tc.expectedType, tok.Type)
+	}
+
+	if tok.Literal != tc.expectedLiteral {
+		t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+			i, tc.expectedLiteral, tok.Literal)
+	}
+}
+
+func TestLexer_Symbols(t *testing.T) {
 	input := `=+(){},;`
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
+	tests := []LexerTestCase{
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
@@ -25,34 +41,14 @@ func TestNextToken(t *testing.T) {
 	l := New(input)
 
 	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
+		tt.Check(l, i, t)
 	}
 }
 
-func TestNextToken_RealSyntax(t *testing.T) {
-	input := `let five = 5;
-let ten = 10;
+func TestLexer_Assignment(t *testing.T) {
+	input := `let five = 5; let ten = 10;`
 
-let add = fn(x,y) {
-	x + y;
-};
-
-let result = add(five, ten);`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
+	tests := []LexerTestCase{
 		{token.LET, "let"},
 		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
@@ -63,88 +59,40 @@ let result = add(five, ten);`
 		{token.ASSIGN, "="},
 		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
-		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
-		{token.COMMA, ","},
-		{token.IDENT, "ten"},
-		{token.RPAREN, ")"},
-		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
 	l := New(input)
 
 	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
+		tt.Check(l, i, t)
 	}
 }
 
-func TestNextToken_MEGA(t *testing.T) {
-	input := `let five = 5;
-let ten = 10;
+func TestLexer_WhitespaceIgnored(t *testing.T) {
+	input := `
 
-let add = fn(x, y) {
-  x + y;
-};
-
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-
-if (5 < 10) {
-	return true;
-} else {
-	return false;
-}
-
-10 == 10;
-10 != 9;
+let   x    =    5    ;        
 `
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
+	tests := []LexerTestCase{
 		{token.LET, "let"},
-		{token.IDENT, "five"},
-		{token.ASSIGN, "="},
+		{token.IDENT, "x"},
+		{token.EQ, "="},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tt.Check(l, i, t)
+	}
+}
+
+func TestLexer_Functions(t *testing.T) {
+	input := `let add = fn(x,y) { x + y; }; let result = add(five, ten);`
+
+	tests := []LexerTestCase{
 		{token.LET, "let"},
 		{token.IDENT, "add"},
 		{token.ASSIGN, "="},
@@ -171,18 +119,20 @@ if (5 < 10) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.GT, ">"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tt.Check(l, i, t)
+	}
+}
+
+func TestLexer_IfStatement(t *testing.T) {
+	input := `if (5 < 10) { return true; } else { return false; }`
+
+	tests := []LexerTestCase{
 		{token.IF, "if"},
 		{token.LPAREN, "("},
 		{token.INT, "5"},
@@ -200,6 +150,31 @@ if (5 < 10) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tt.Check(l, i, t)
+	}
+}
+func TestLexer_Operators(t *testing.T) {
+	input := `!-/*5; 5 < 10 > 5; 10 == 10; 10 != 9;`
+
+	tests := []LexerTestCase{
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.INT, "10"},
 		{token.EQ, "=="},
 		{token.INT, "10"},
@@ -214,16 +189,6 @@ if (5 < 10) {
 	l := New(input)
 
 	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
+		tt.Check(l, i, t)
 	}
 }
